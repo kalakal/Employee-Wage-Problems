8d14c3364e5e78227ac44877f530075be5e2433f
kalakal <sayedfaizanqadri@gmail.com>
2023-02-28 23:41:10 +0530

    Added : Employee monthly salary

--- src/com/bridgelab/emloyeeWage/EmployeeWageComputation.java
+++ src/com/bridgelab/emloyeeWage/EmployeeWageComputation.java
@@ -1,44 +1,43 @@
 package com.bridgelab.emloyeeWage;
 
-//UC-4 Employee daily wage using Switch case statement
+//UC-5 Employee Monthly wage for 20 working days
 public class EmployeeWageComputation {
     static int isFullTime = 1;
-    static int isPartTime =2;
-    static int wagePerHr =20;
-    private String dailyWage;
+    static int isPartTime = 2;
+    static int wagePerHr = 20;
+    static int maxWorkDay =20;
 
-
-    public int attaindance() {
-        double empAttain = Math.floor(Math.random() * 10) % 3;
-        if (empAttain == isFullTime)
-            System.out.println("Employee is Full time Present");
-        else if (empAttain == isPartTime)
-            System.out.println("Employee is Part time Present");
-        else
-            System.out.println("Employee is Absent");
-        return (int) empAttain;
+    public void display() {
+        System.out.println("Well come to Employee wage computation program");
     }
-    public void dailyWage(int empAttain) {
-        int workingHour=0;
-        switch (empAttain){
-            case 1:
-                workingHour = 8;
-                break;
-            case 2:
-                workingHour = 4;
-                break;
-            default:
-        }
-        int wage = workingHour * wagePerHr;
 
-        System.out.println(" wage of employee: " +wage);
+
+    public void monthWage() {
+        int workingHour = 0;
+        int totalWage=0;
+        int totalWorkingHour=0;
+        for (int day = 1; day<=maxWorkDay; day++) {
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            switch (empCheck) {
+                case 1:
+                    workingHour = 8;
+                    break;
+                case 2:
+                    workingHour = 4;
+                    break;
+                default:
+            }
+            totalWorkingHour = totalWorkingHour + workingHour;
+            int wage = workingHour * wagePerHr;
+            totalWage = totalWage + wage;
+        }
+        System.out.println("Monthly wage of employee: " + totalWage+ "    Total  Working hour is: " +totalWorkingHour);
     }
 
     public static void main(String[] args) {
-        System.out.println("Well come to Employee wage computation program");
         EmployeeWageComputation employee = new EmployeeWageComputation();
-        int empAttain=employee.attaindance();
-        employee.dailyWage(empAttain);
+        employee.display();
+        employee.monthWage();
     }
 
 }
