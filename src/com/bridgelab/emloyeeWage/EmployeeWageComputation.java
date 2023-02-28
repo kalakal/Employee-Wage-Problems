56edd81eb03b7a58a9fd9cf1e155f9c6f1fd5710
kalakal <sayedfaizanqadri@gmail.com>
2023-02-28 23:26:16 +0530

    Added : Employee Daily Wage FullTime and part time

--- src/com/bridgelab/emloyeeWage/EmployeeWageComputation.java
+++ src/com/bridgelab/emloyeeWage/EmployeeWageComputation.java
@@ -1,23 +1,31 @@
 package com.bridgelab.emloyeeWage;
 
 public class EmployeeWageComputation {
-
     static int isFullTime = 1;
+    static int isPartTime =2;
     static int wagePerHr =20;
     private String dailyWage;
 
 
     public int attaindance() {
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == isFullTime)
-            System.out.println("Employee is Present");
+        double empAttain = Math.floor(Math.random() * 10) % 3;
+        if (empAttain == isFullTime)
+            System.out.println("Employee is Full time Present");
+        else if (empAttain == isPartTime)
+            System.out.println("Employee is Part time Present");
         else
             System.out.println("Employee is Absent");
-        return (int) empCheck;
+        return (int) empAttain;
     }
-    public void dailyWage(int empStatus) {
-        if (empStatus == isFullTime){
+    public void dailyWage(int empAttain) {
+
+        if (empAttain == isFullTime){
             int workingHour=8;
+            int dailyWage= wagePerHr*workingHour;
+            System.out.println("Employee daily wage : " +dailyWage);
+        }
+        else if (empAttain == isPartTime){
+            int workingHour=4;
             int dailyWage= wagePerHr*workingHour;
             System.out.println("Employee daily wage : " +dailyWage);
         }
@@ -32,8 +40,8 @@ public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("Well come to Employee wage computation program");
         EmployeeWageComputation employee = new EmployeeWageComputation();
-        int empStatus=employee.attaindance();
-        employee.dailyWage(empStatus);
+        int empAttain=employee.attaindance();
+        employee.dailyWage(empAttain);
     }
 
 }
