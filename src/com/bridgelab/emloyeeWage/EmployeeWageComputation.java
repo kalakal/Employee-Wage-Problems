88b36a19d3e371e5343359d9fbe8df67a1524108
kalakal <sayedfaizanqadri@gmail.com>
2023-03-03 19:41:10 +0530

    [Added] EmployWageUsing OOPS Concept

--- src/com/bridgelab/emloyeeWage/EmployeeWageComputation.java
+++ src/com/bridgelab/emloyeeWage/EmployeeWageComputation.java
@@ -1,22 +1,23 @@
 package com.bridgelab.emloyeeWage;
 
-//UC-5 Employee Monthly wage for 20 working days
+//UC-7 Employee Monthly wage for 20 working days and 100 hours using class object and method.
+
 public class EmployeeWageComputation {
     static int isFullTime = 1;
     static int isPartTime = 2;
     static int wagePerHr = 20;
-    static int maxWorkDay =20;
+    static int maxWorkDay = 20;
     static int maxWorkHr = 100;
+    static int totalWorkingHour = 0;
 
     public void display() {
         System.out.println("Well come to Employee wage computation program");
     }
+
     public void monthWage() {
         int workingHour = 0;
-        int totalWage=0;
-        int totalWorkingHour=0;
-        for (int day = 1; day < maxWorkDay
-                && totalWorkingHour < maxWorkHr; day++ ) {
+        int totalWage = 0;
+        for (int day = 1; day < maxWorkDay && totalWorkingHour < maxWorkHr; day++) {
             int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
             switch (empCheck) {
                 case 1:
@@ -28,10 +29,15 @@ public class EmployeeWageComputation {
                 default:
             }
             totalWorkingHour = totalWorkingHour + workingHour;
-            int wage = workingHour * wagePerHr;
-            totalWage = totalWage + wage;
+            if (totalWorkingHour > maxWorkHr) {
+                totalWorkingHour = maxWorkHr;
+            }
         }
-        System.out.println("Monthly wage of employee: " + totalWage+ "    Total  Working hour is: " +totalWorkingHour);
+
+        totalWage = totalWorkingHour * wagePerHr;
+
+        System.out
+                .println("Monthly wage of employee: " + totalWage + "    Total  Working hour is: " + totalWorkingHour);
     }
 
     public static void main(String[] args) {
